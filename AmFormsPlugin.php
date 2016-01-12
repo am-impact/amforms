ffdeed1d1d8e4973d70300c585cc37afa5e0645b
Hubert Prein <h.prein@am-impact.nl>
2016-01-12 08:23:14 +0100

    Add table option when creating fields

--- AmFormsPlugin.php
+++ AmFormsPlugin.php
@@ -28,7 +28,7 @@ class AmFormsPlugin extends BasePlugin
      */
     public function getVersion()
     {
-        return '1.3.2';
+        return '1.3.3';
     }
 
     /**
@@ -36,7 +36,7 @@ class AmFormsPlugin extends BasePlugin
      */
     public function getSchemaVersion()
     {
-        return '1.3.2';
+        return '1.3.3';
     }
 
     /**
